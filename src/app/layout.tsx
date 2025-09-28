a8b7c88e80b673295a1a060e7e243960665069ff
Eric Maya <ericmaya8a@gmail.com>
2025-09-28 15:36:09 -0600

    chore: add navigation component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import { ThemeToggle } from '@/components/common/theme-toggle/theme-toggle';
+import { MainNavigation } from '@/components/common/main-navigation';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { themeMapping } from '@/theme';
 import './globals.css';
@@ -36,9 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
           themes={Object.keys(themeMapping)}
         >
-          <nav>
-            <ThemeToggle />
-          </nav>
+          <MainNavigation />
           {children}
         </ThemeProvider>
       </body>
---
+++ src/components/common/main-navigation/index.tsx
@@ -0,0 +1,9 @@
+import { ThemeToggle } from '@/components/common/theme-toggle/theme-toggle';
+
+export function MainNavigation() {
+  return (
+    <nav className="p-4">
+      <ThemeToggle />
+    </nav>
+  );
+}
