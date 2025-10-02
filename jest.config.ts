3205cfaeab8844bd525bfe6b121c6239c1be4955
Eric Maya <ericmaya8a@gmail.com>
2025-10-01 18:29:39 -0600

    test: dd lyout test

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coverageProvider: 'v8',
-  coveragePathIgnorePatterns: ['/node_modules/', 'index.ts', 'layout.tsx', '/shadcn', '/types'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index.ts', '/shadcn', '/types'],
   coverageThreshold: {
     global: {
       branches: 80,
--- src/app/layout.test.tsx
+++ src/app/layout.test.tsx
@@ -1,18 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import { ReactNode } from 'react';
 
 import RootLayout from './layout';
 
-jest.mock('./layout', () => ({
-  __esModule: true,
-  default: ({ children }: { children: ReactNode }) => <main data-testid="theme-provider">{children}</main>,
+jest.mock('@/components/providers/theme-provider', () => ({
+  ThemeProvider: ({ children }: { children: React.ReactNode }) => (
+    <div data-testid="mock-theme-provider">{children}</div>
+  ),
 }));
 
 describe('RootLayout', () => {
   it('renders children and main navigation', () => {
     render(<RootLayout>Test Child</RootLayout>);
 
-    expect(screen.getByRole('main')).toBeInTheDocument();
+    expect(screen.getByTestId('mock-theme-provider')).toBeInTheDocument();
     expect(screen.getByText('Test Child')).toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('main')).toBeInTheDocument();
   });
 });
