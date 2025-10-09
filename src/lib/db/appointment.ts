18ebe598223dd8dbf2c3ea3695bf8cbaa4de7099
Eric Maya <ericmaya8a@gmail.com>
2025-10-09 10:57:19 -0600

    feat: add appointment db crud

--- src/lib/db/appointment.ts
+++ src/lib/db/appointment.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@/generated/prisma';
 import prisma from '@/lib/prisma';
 
 export async function getAppointmentsByDate(startDate: Date, endDate: Date) {
@@ -12,4 +13,26 @@ export async function getAppointmentsByDate(startDate: Date, endDate: Date) {
       AND: [{ date: { gte: startDate } }, { date: { lte: endDate } }],
     },
   });
+}
+
+export async function findAppointmentById(appointmentId: number) {
+  return await prisma.appointment.findUnique({
+    where: { id: appointmentId },
+    include: {
+      Patient: { select: { allergies: true, firstName: true, lastName: true, notes: true } },
+      prescriptions: true,
+    },
+  });
+}
+
+export async function createAppointment(data: Prisma.AppointmentCreateInput) {
+  return await prisma.appointment.create({ data });
+}
+
+export async function updateAppointment(appointmentId: number, data: Prisma.AppointmentUpdateInput) {
+  return await prisma.appointment.update({ where: { id: appointmentId }, data });
+}
+
+export async function deleteAppointment(appointmentId: number) {
+  return await prisma.appointment.delete({ where: { id: appointmentId } });
 }
